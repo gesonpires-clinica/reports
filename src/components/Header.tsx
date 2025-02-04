d689a533c1d9f7aa7f89f38cd5a125ca13df6d6e
gesonpires-clinica <clinicaneuromarianebach@gmail.com>
2025-02-04 14:03:31 -0300

    Estilo hover.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,26 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-blue-600 text-white p-4 flex items-center justify-between">
+    <header className="bg-blue-600 text-white p-4 flex items-center justify-between shadow-md">
       <div className="text-xl font-bold">
-        <Link href="/">GeraRAN</Link>
+        <Link
+          href="/"
+          className="transition-colors duration-200 hover:to-gray-300 hover:scale-105"
+        >
+          GeraRAN
+        </Link>
       </div>
       <nav className="space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/editor">Criar Relatório</Link>
+        <Link
+          href="/"
+          className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
+        >
+          Home
+        </Link>
+        <Link href="/dashboard"          className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
+        >Painel</Link>
+        <Link href="/editor"           className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
+        >Criar Relatório</Link>
       </nav>
     </header>
   );
