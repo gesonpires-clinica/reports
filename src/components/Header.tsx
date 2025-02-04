e5879695655b5ec81770b88444780faf2e0f052c
gesonpires-clinica <clinicaneuromarianebach@gmail.com>
2025-02-04 16:10:44 -0300

    Revisão da responsividade.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,46 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="bg-blue-600 text-white p-4 flex items-center justify-between shadow-md">
-      <div className="text-xl font-bold">
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        {/* Área do Logo e Título */}
+        <Image
+          src="/logo-mb.png"
+          alt="Logo GeraRAN"
+          width={40} // ajuste a largura conforme necessário
+          height={40} // ajuste a altura conforme necessário
+        />
         <Link
           href="/"
-          className="transition-colors duration-200 hover:to-gray-300 hover:scale-105"
-        >
+          className="text-xl font-bold transition-colors duration-200 hover:bg-blue-700 px-2 py-1"
+          >
           GeraRAN
         </Link>
       </div>
-      <nav className="space-x-4">
+      {/* Navegação */}
+      <nav className="mt-4 md:mt-0 w-full flex flex-col md:flex-row md:justify-end space-y-2 md:space-y-0 md:space-x-4">
         <Link
           href="/"
-          className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
-        >
+          className="transition-colors duration-200 hover:bg-blue-700 px-2 py-1"
+          >
           Home
         </Link>
-        <Link href="/dashboard"          className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
-        >Painel</Link>
-        <Link href="/editor"           className="transition-colors duration-200 hover:text-gray-300 hover:scale-105"
-        >Criar Relatório</Link>
+        <Link
+          href="/dashboard"
+          className="transition-colors duration-200 hover:bg-blue-700 px-2 py-1"
+          >
+          Painel
+        </Link>
+        <Link
+          href="/editor"
+          className="transition-colors duration-200 hover:bg-blue-700 px-2 py-1"
+          >
+          Criar Relatório
+        </Link>
       </nav>
     </header>
   );
