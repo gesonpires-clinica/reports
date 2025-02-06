fe91f1274e2c316e950b2d0cfe99af3fa0a6dade
gesonpires-clinica <clinicaneuromarianebach@gmail.com>
2025-02-06 13:27:03 -0300

    Criei app/head.tsx para título e favicon.

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Meu Projeto - Título da Aba</title>
+        <meta name="description" content="Descrição do meu projeto." />
+        <link rel="icon" href="/favicon.png" />
+      </>
+    );
+  }
+  
